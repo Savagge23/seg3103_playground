e1fb4d4293684a6f20fe9aba20f6c15bcd34b3b2
harjotsandhu <hsand096>
2024-06-13 14:50:02 -0400

    TC01 Passed

--- src/main/TicTacToe.java
+++ src/main/TicTacToe.java
@@ -4,7 +4,12 @@ public class TicTacToe {
     private static String table[][];
 
     public TicTacToe(){
-
+        table = new String[3][3];
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                table[i][j] = " ";
+            }
+        }
     }
 
     public static String getCell(int i, int j){
