6962a9718e8c78f7894253a659a5e6e122f2adc0
harjotsandhu <hsand096>
2024-06-13 14:39:07 -0400

    Inital Commit Failed TC01

---
+++ src/main/TicTacToe.java
@@ -0,0 +1,13 @@
+package main;
+
+public class TicTacToe {
+    private static String table[][];
+
+    public TicTacToe(){
+
+    }
+
+    public static String getCell(int i, int j){
+        return table[i][j];
+    }
+}
---
+++ src/test/TicTacToeTest.java
@@ -0,0 +1,19 @@
+package test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import main.TicTacToe;
+import org.junit.jupiter.api.Test;
+
+public class TicTacToeTest {
+
+    @Test
+    void newGame_tc01(){
+        TicTacToe newGame = new TicTacToe();
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                assertEquals(" ", TicTacToe.getCell(i,j));
+            }
+        }
+    }
+}
