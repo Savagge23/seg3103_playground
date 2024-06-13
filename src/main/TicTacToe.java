812af8d6056cc8e87f87addd383bac9806aeea43
harjotsandhu <hsand096>
2024-06-13 16:02:28 -0400

    Refactor TC05

--- src/main/TicTacToe.java
+++ src/main/TicTacToe.java
@@ -3,35 +3,35 @@ package main;
 public class TicTacToe {
     private static String table[][];
 
-    public TicTacToe(){
+    public TicTacToe() {
         table = new String[3][3];
         loadTable();
     }
 
-    private void loadTable(){
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
+    private void loadTable() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 table[i][j] = " ";
             }
         }
     }
 
-    public static String getCell(int i, int j){
+    public static String getCell(int i, int j) {
         return table[i][j];
     }
 
-    public void playX(int i, int j){
+    public void playX(int i, int j) {
         table[i][j] = "X";
     }
 
-    public void playO(int i, int j){
+    public void playO(int i, int j) {
         table[i][j] = "O";
     }
 
-    public static boolean tableisFull(){
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
-                if(table[i][j].equals(" ")){
+    public static boolean tableisFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (table[i][j].equals(" ")) {
                     return false;
                 }
             }
@@ -39,12 +39,25 @@ public class TicTacToe {
         return true;
     }
 
-    public static boolean isGameWon(){
-        if(table[0][1] == "X" && table[1][1] == "X" && table[2][1] == "X"){
+    public static boolean isGameWon() {
+        for (int i = 0; i < 3; i++) {
+            if (((table[i][0] == ("X") && table[i][1] == ("X") && table[i][2] == ("X")) ||
+                    (table[i][0] == ("O") && table[i][1] == ("O") && table[i][2] == ("O"))) ||
+                    ((table[0][i] == ("O") && table[1][i] == ("O") && table[2][i] == ("O")) ||
+                            (table[0][i] == ("X") && table[1][i] == ("X") && table[2][i] == ("X"))))
+                return true;
+            }
+        if((table[0][0] == "X" && table[1][1] == "X" && table[2][2] == "X") ||
+                (table[0][0] == "O" && table[1][1] == "O" && table[2][2] == "O") ||
+                (table[0][2] == "X" && table[1][1] == "X" && table[2][0] == "X") ||
+                (table[0][2] == "O" && table[1][1] == "O" && table[2][0] == "O")){
             return true;
         }
-        return false;
+
+            return false;
+        }
+
+
     }
 
 
-}
